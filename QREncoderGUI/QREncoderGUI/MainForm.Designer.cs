aafd014d1ff87f13dec7f75cc9ea5bd9e5d260aa
Blaise Wang <blaisewang@icloud.com>
2017-04-19 23:00:09 +0800

    update code file

---
+++ QREncoderGUI/QREncoderGUI/MainForm.Designer.cs
@@ -0,0 +1,175 @@
+﻿using System.Windows.Forms;
+
+namespace QREncoderGUI
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.buttonGenerate = new System.Windows.Forms.Button();
+            this.textBoxFileName = new System.Windows.Forms.TextBox();
+            this.radioButtonColorWhite = new System.Windows.Forms.RadioButton();
+            this.radioButtonColorBlack = new System.Windows.Forms.RadioButton();
+            this.groupBoxColor = new System.Windows.Forms.GroupBox();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.comboBoxTable = new System.Windows.Forms.ComboBox();
+            this.labelHint = new System.Windows.Forms.Label();
+            this.groupBoxColor.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonLoad.Location = new System.Drawing.Point(323, 12);
+            this.buttonLoad.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(90, 30);
+            this.buttonLoad.TabIndex = 0;
+            this.buttonLoad.Text = "Load";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.ButtonLoad_Click);
+            // 
+            // buttonGenerate
+            // 
+            this.buttonGenerate.Location = new System.Drawing.Point(323, 130);
+            this.buttonGenerate.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(90, 30);
+            this.buttonGenerate.TabIndex = 1;
+            this.buttonGenerate.Text = "Generate";
+            this.buttonGenerate.UseVisualStyleBackColor = true;
+            this.buttonGenerate.Click += new System.EventHandler(this.ButtonGenerate_Click);
+            // 
+            // textBoxFileName
+            // 
+            this.textBoxFileName.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.textBoxFileName.Location = new System.Drawing.Point(15, 15);
+            this.textBoxFileName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBoxFileName.Name = "textBoxFileName";
+            this.textBoxFileName.ReadOnly = true;
+            this.textBoxFileName.Size = new System.Drawing.Size(300, 23);
+            this.textBoxFileName.TabIndex = 2;
+            this.textBoxFileName.Text = "Select LOGO File (Optional)";
+            // 
+            // radioButtonColorWhite
+            // 
+            this.radioButtonColorWhite.AutoSize = true;
+            this.radioButtonColorWhite.Checked = true;
+            this.radioButtonColorWhite.Location = new System.Drawing.Point(10, 25);
+            this.radioButtonColorWhite.Name = "radioButtonColorWhite";
+            this.radioButtonColorWhite.Size = new System.Drawing.Size(59, 21);
+            this.radioButtonColorWhite.TabIndex = 4;
+            this.radioButtonColorWhite.TabStop = true;
+            this.radioButtonColorWhite.Text = "White";
+            this.radioButtonColorWhite.UseVisualStyleBackColor = true;
+            this.radioButtonColorWhite.CheckedChanged += new System.EventHandler(this.RadioButtonColorWhite_CheckedChanged);
+            // 
+            // radioButtonColorBlack
+            // 
+            this.radioButtonColorBlack.AutoSize = true;
+            this.radioButtonColorBlack.Location = new System.Drawing.Point(108, 25);
+            this.radioButtonColorBlack.Name = "radioButtonColorBlack";
+            this.radioButtonColorBlack.Size = new System.Drawing.Size(57, 21);
+            this.radioButtonColorBlack.TabIndex = 5;
+            this.radioButtonColorBlack.TabStop = true;
+            this.radioButtonColorBlack.Text = "Black";
+            this.radioButtonColorBlack.UseVisualStyleBackColor = true;
+            this.radioButtonColorBlack.CheckedChanged += new System.EventHandler(this.RadioButtonColorBlack_CheckedChanged);
+            // 
+            // groupBoxColor
+            // 
+            this.groupBoxColor.Controls.Add(this.radioButtonColorBlack);
+            this.groupBoxColor.Controls.Add(this.radioButtonColorWhite);
+            this.groupBoxColor.Location = new System.Drawing.Point(15, 110);
+            this.groupBoxColor.Name = "groupBoxColor";
+            this.groupBoxColor.Size = new System.Drawing.Size(170, 55);
+            this.groupBoxColor.TabIndex = 6;
+            this.groupBoxColor.TabStop = false;
+            this.groupBoxColor.Text = "Background Color";
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(208, 130);
+            this.buttonReset.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(90, 30);
+            this.buttonReset.TabIndex = 9;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.ButtonReset_Click);
+            // 
+            // comboBoxTable
+            // 
+            this.comboBoxTable.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxTable.FormattingEnabled = true;
+            this.comboBoxTable.Location = new System.Drawing.Point(15, 65);
+            this.comboBoxTable.Name = "comboBoxTable";
+            this.comboBoxTable.Size = new System.Drawing.Size(300, 25);
+            this.comboBoxTable.TabIndex = 10;
+            this.comboBoxTable.SelectedIndexChanged += new System.EventHandler(this.ComboBoxTableName_SelectedIndexChanged);
+            // 
+            // labelHint
+            // 
+            this.labelHint.AutoSize = true;
+            this.labelHint.ForeColor = System.Drawing.Color.Red;
+            this.labelHint.Location = new System.Drawing.Point(322, 70);
+            this.labelHint.Name = "labelHint";
+            this.labelHint.Size = new System.Drawing.Size(0, 17);
+            this.labelHint.TabIndex = 11;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 176);
+            this.Controls.Add(this.labelHint);
+            this.Controls.Add(this.comboBoxTable);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.groupBoxColor);
+            this.Controls.Add(this.textBoxFileName);
+            this.Controls.Add(this.buttonGenerate);
+            this.Controls.Add(this.buttonLoad);
+            this.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "QREncoder";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.groupBoxColor.ResumeLayout(false);
+            this.groupBoxColor.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button buttonLoad;
+        private Button buttonGenerate;
+        private TextBox textBoxFileName;
+        private RadioButton radioButtonColorWhite;
+        private RadioButton radioButtonColorBlack;
+        private GroupBox groupBoxColor;
+        private Button buttonReset;
+        private ComboBox comboBoxTable;
+        private Label labelHint;
+    }
+}
+
---
+++ QREncoderGUI/QREncoderGUI/MainForm.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using ZXing;
+using ZXing.QrCode;
+using ZXing.QrCode.Internal;
+
+namespace QREncoderGUI
+{
+    public partial class MainForm : Form
+    {
+        private const BarcodeFormat DEFAULT_BARCODE_FORMAT = BarcodeFormat.QR_CODE;
+        private static readonly ImageFormat DEFAULT_IMAGE_FORMAT = ImageFormat.Png;
+        private const string DEFAULT_OUTPUT_DIRECTORY = @"QRCodes.out\";
+        private const int DEFAULT_WIDTH = 600;
+        private const int DEFAULT_HEIGHT = 600;
+        private const string DEFAULT_SELECT_LOGO_HINT = "Select LOGO File (Optional)";
+
+        private Bitmap logo;
+        private bool isBackgroundColorWhite = true;
+
+        private object thisLock = new object();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonLoad_Click(object sender, EventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog()
+            {
+                Filter = "All Image Files|*.jpeg;*.jpg;*.png;|" +
+                                    "JPEG (*.jpg)|*.jpg;*.jpeg|" +
+                                    "PNG (*.png)|*.png"
+            };
+            if (DialogResult.OK == openFileDialog.ShowDialog(this))
+            {
+                var img = Image.FromFile(openFileDialog.FileName);
+                if (img != null)
+                {
+                    radioButtonColorWhite.Enabled = true;
+                    radioButtonColorBlack.Enabled = true;
+                    logo = new Bitmap(img);
+                    textBoxFileName.Text = openFileDialog.SafeFileName;
+                }
+            }
+        }
+
+        private void ButtonGenerate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var dataTable = new DataTable();
+                var dataColumnKey = new DataColumn("key", typeof(int));
+                var dataColumnValue = new DataColumn("value", typeof(string));
+                dataTable.Columns.Add(dataColumnKey);
+                dataTable.Columns.Add(dataColumnValue);
+                // add your data here
+
+                labelHint.Text = "";
+
+                Directory.CreateDirectory(DEFAULT_OUTPUT_DIRECTORY);
+
+                int n = dataTable.Rows.Count;
+                int stride = 50;
+                for (int i = 0; i < n; i += stride)
+                {
+                    Parallel.For(i, Math.Min(i + stride, n), j =>
+                    {
+                        GenerateQRCode(dataTable.Rows[j]);
+                    });
+                }
+                System.Diagnostics.Process.Start("explorer", DEFAULT_OUTPUT_DIRECTORY);
+            }
+            catch (Exception exception)
+            {
+                labelHint.Text = "Not Found";
+                Console.WriteLine(DateTime.Now + " " + exception.Message);
+            }
+        }
+
+        private void GenerateQRCode(DataRow dataRows)
+        {
+            var barcodeFormat = DEFAULT_BARCODE_FORMAT;
+            var imageFormat = DEFAULT_IMAGE_FORMAT;
+            var outFileString = DEFAULT_OUTPUT_DIRECTORY + dataRows["key"].ToString();
+            var width = DEFAULT_WIDTH;
+            var height = DEFAULT_HEIGHT;
+
+            outFileString += '.' + imageFormat.ToString();
+
+            //TODO: change your qrcode format here
+            string contents = "key:" + dataRows["key"].ToString() + " value:" + dataRows["value"].ToString();
+
+            var barcodeWriter = new BarcodeWriter
+            {
+                Format = barcodeFormat,
+                Options = new QrCodeEncodingOptions
+                {
+                    ErrorCorrection = ErrorCorrectionLevel.M,
+                    Height = height,
+                    Width = width
+                }
+            };
+
+            var bitmap = barcodeWriter.Write(contents);
+
+            if (logo != null)
+            {
+                int backgroundWidth = 10;
+                int backgroundHeight = 10;
+                lock (thisLock)
+                {
+                    backgroundWidth += logo.Width;
+                    backgroundHeight += logo.Height;
+                }
+
+                var background = new Bitmap(backgroundWidth, backgroundHeight);
+                var graphics = Graphics.FromImage(background);
+                if (isBackgroundColorWhite)
+                {
+                    FillRoundRectangle(graphics, Brushes.White, new Rectangle(0, 0, backgroundWidth, backgroundHeight), 10);
+                }
+                else
+                {
+                    FillRoundRectangle(graphics, Brushes.Black, new Rectangle(0, 0, backgroundWidth, backgroundHeight), 10);
+                }
+                graphics.Dispose();
+                lock (thisLock)
+                {
+                    bitmap = OverlayImage(background, bitmap);
+                    bitmap = OverlayImage(logo, bitmap);
+                }
+            }
+
+            bitmap.Save(outFileString, imageFormat);
+            bitmap.Dispose();
+        }
+
+        private static Bitmap OverlayImage(Bitmap foregroundImage, Bitmap backgroundImage)
+        {
+            var newBitmap = new Bitmap(backgroundImage.Width, backgroundImage.Height);
+            var graphics = Graphics.FromImage(newBitmap);
+            graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            graphics.CompositingQuality = CompositingQuality.HighQuality;
+
+            graphics.DrawImage(backgroundImage, 0, 0);
+            graphics.DrawImage(foregroundImage, (backgroundImage.Width - foregroundImage.Width) / 2, (backgroundImage.Height - foregroundImage.Height) / 2);
+            graphics.Dispose();
+
+            return newBitmap;
+        }
+
+        public static void FillRoundRectangle(Graphics g, Brush brush, Rectangle rect, int cornerRadius)
+        {
+            using (GraphicsPath path = CreateRoundedRectanglePath(rect, cornerRadius))
+            {
+                g.SmoothingMode = SmoothingMode.AntiAlias;
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                g.CompositingQuality = CompositingQuality.HighQuality;
+                g.FillPath(brush, path);
+            }
+        }
+
+        internal static GraphicsPath CreateRoundedRectanglePath(Rectangle rect, int cornerRadius)
+        {
+            var roundedRect = new GraphicsPath();
+            roundedRect.AddArc(rect.X, rect.Y, cornerRadius * 2, cornerRadius * 2, 180, 90);
+            roundedRect.AddLine(rect.X + cornerRadius, rect.Y, rect.Right - cornerRadius * 2, rect.Y);
+            roundedRect.AddArc(rect.X + rect.Width - cornerRadius * 2, rect.Y, cornerRadius * 2, cornerRadius * 2, 270, 90);
+            roundedRect.AddLine(rect.Right, rect.Y + cornerRadius * 2, rect.Right, rect.Y + rect.Height - cornerRadius * 2);
+            roundedRect.AddArc(rect.X + rect.Width - cornerRadius * 2, rect.Y + rect.Height - cornerRadius * 2, cornerRadius * 2, cornerRadius * 2, 0, 90);
+            roundedRect.AddLine(rect.Right - cornerRadius * 2, rect.Bottom, rect.X + cornerRadius * 2, rect.Bottom);
+            roundedRect.AddArc(rect.X, rect.Bottom - cornerRadius * 2, cornerRadius * 2, cornerRadius * 2, 90, 90);
+            roundedRect.AddLine(rect.X, rect.Bottom - cornerRadius * 2, rect.X, rect.Y + cornerRadius * 2);
+            roundedRect.CloseFigure();
+            return roundedRect;
+        }
+
+        private void RadioButtonColorWhite_CheckedChanged(object sender, EventArgs e)
+        {
+            isBackgroundColorWhite = true;
+        }
+
+        private void RadioButtonColorBlack_CheckedChanged(object sender, EventArgs e)
+        {
+            isBackgroundColorWhite = false;
+        }
+
+        internal void InitializeComboBox()
+        {
+            try
+            {
+                var dataTable = new DataTable();
+                var dataColumnKey = new DataColumn("listValue", typeof(Int64));
+                var dataColumnValue = new DataColumn("listName", typeof(String));
+                dataTable.Columns.Add(dataColumnKey);
+                dataTable.Columns.Add(dataColumnValue);
+
+                //TODO: add value and name of combo here
+
+                comboBoxTable.ValueMember = "listValue";
+                comboBoxTable.DisplayMember = "listName";
+                comboBoxTable.DataSource = dataTable;
+
+                buttonGenerate.Enabled = true;
+                buttonLoad.Enabled = true;
+                labelHint.Text = "";
+            }
+            catch (Exception)
+            {
+                buttonGenerate.Enabled = false;
+                buttonLoad.Enabled = false;
+                labelHint.Text = "Error Initializing";
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            radioButtonColorWhite.Enabled = false;
+            radioButtonColorBlack.Enabled = false;
+
+            InitializeComboBox();
+        }
+
+        private void ButtonReset_Click(object sender, EventArgs e)
+        {
+            InitializeComboBox();
+            if (logo != null)
+            {
+                logo.Dispose();
+                logo = null;
+                if (!isBackgroundColorWhite)
+                {
+                    radioButtonColorWhite.Select();
+                    isBackgroundColorWhite = true;
+                }
+                radioButtonColorWhite.Enabled = false;
+                radioButtonColorBlack.Enabled = false;
+                textBoxFileName.Text = DEFAULT_SELECT_LOGO_HINT;
+            }
+        }
+
+        private void ComboBoxTableName_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBoxTable.Items.Count > 0)
+            {
+                //TODO: handle option here
+            }
+        }
+    }
+}
